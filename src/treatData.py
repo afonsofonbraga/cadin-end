46013be18a168eb333232e5280a7f6c8525733e8
Vitim. .Axlash <giudice.vitor@engenharia.ufjf.br>
2021-10-02 20:01:51 -0300

    latitude longitude elon musk

--- src/treatData.py
+++ src/treatData.py
@@ -4,6 +4,10 @@ from sgp4.functions import jday
 from query import insert_debri
 from Debris import Debris
 
+import astropy.time
+from astropy.coordinates import TEME, CartesianDifferential, CartesianRepresentation, ITRS
+from astropy import units as u
+
 def importDebris(time):
     try:
         con = sqlite3.connect('../data/database.db')
@@ -32,10 +36,26 @@ def importDebris(time):
             hour = int(time.split(',')[3])
             minute = int(time.split(',')[4])
             second = int(time.split(',')[5])
+
             jd, fr = jday(year, month, day, hour, minute, second)
             e, r, v = satellite.sgp4(jd, fr)
+
             if e == 0:
                 debris_count += 1
+
+                time = astropy.time.Time(jd + fr, format = 'jd')
+                r = CartesianRepresentation(r*u.km)
+                v = CartesianDifferential(v*u.km/u.s)
+                teme = TEME(r.with_differentials(v), obstime=time)
+                itrs = teme.transform_to(ITRS(obstime=time))
+                location = itrs.earth_location
+
+                longitude = int(str(location.geodetic.lon).split('d')[0])
+                latitude = int(str(location.geodetic.lat).split('d')[0])
+                height = float(str(location.geodetic.height).split('km')[0])
+
+                r = [longitude, latitude, height]
+
                 new_debri = Debris(row[0],r,v)
                 insert_debri(new_debri)
         print("From", len(records), "tle objects,", debris_count,"debris were added into the database.")

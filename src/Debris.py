5f495afd90debf3578070b673c4f432a59aa77f7
Vitim. .Axlash <giudice.vitor@engenharia.ufjf.br>
2021-10-02 13:13:21 -0300

    Changed Debris and TLE2

--- src/Debris.py
+++ src/Debris.py
@@ -1,8 +1,12 @@
 class Debris:
     def __init__(self, name, pos, vel) -> None:
         self.name = name
-        self.position = pos
-        self.velocity = vel
+        self.pos1 = pos[0]
+        self.pos2 = pos[1]
+        self.pos3 = pos[2]
+        self.vel1 = vel[0]
+        self.vel2 = vel[1]
+        self.vel3 = vel[2]
 
     def __repr__(self): 
         return "Name:  % s \nPosition: % s \nVelocity: % s \n" % (self.name, self.position, self.velocity)

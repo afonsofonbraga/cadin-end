ca5a87b573352c0ed0b0afefa94ebcaa2dec66fe
Vitim. .Axlash <giudice.vitor@engenharia.ufjf.br>
2021-10-02 13:02:23 -0300

    Changed Debris

--- src/Debris.py
+++ src/Debris.py
@@ -1,14 +1,8 @@
 class Debris:
-    def init(self, name, s, t) -> None:
+    def __init__(self, name, s, t) -> None:
         self.name = name
         self.s = s
         self.t = t
-
-    # def repr(self):
-    #     # return "Debri" + self.name
-    #     # print("Line 1" + self.s)
-    #     # print("Line 2" + self.t)
-    #     return f'Debri {self.name}'
-
-    def repr(self): 
-        return "Name:% s \n Line1:% s \n Line2:% s \n" % (self.name, self.s, self.t)
+        
+    def __repr__(self): 
+        return "Name:  % s \nLine1: % s \nLine2: % s \n" % (self.name, self.s, self.t)

05e4ab723346ebf32988218db2bce373119393c4
Afonso Braga <afonso.braga@engenharia.ufjf.br>
2021-10-02 18:32:06 -0300

    add get_dibris_name

--- src/query.py
+++ src/query.py
@@ -36,6 +36,26 @@ def get_debris_id(debris_name):
     con.close()
     return records[0][0]
 
+def get_debris_name(debris_id):
+    con = sqlite3.connect('../data/database.db')
+    cur = con.cursor()
+    try:
+        sqlite_select_query = """SELECT * FROM debris_name WHERE name_id=?"""
+        cur.execute(sqlite_select_query,(debris_id,))
+        con.commit()
+        records = cur.fetchall()
+        con.close()
+    except:
+        con.close()
+        print("Failed to get debris name.")
+        return None
+    if np.size(records) != 0:
+        print("Found")
+        return records[0][1]
+    else:
+        print("Empty debris name search.")
+        return None
+
 def insert_debri(debri):
     debris_id = 0
     try:
@@ -46,17 +66,9 @@ def insert_debri(debri):
     try:
         con = sqlite3.connect('../data/database.db')
         cur = con.cursor()
-        cur.execute('''INSERT OR REPLACE INTO debris (debris_name_id, pos1, pos2, pos3, vel1, vel2, vel3) VALUES (?,?,?,?,?,?,?)''',(debris_id,debri.pos1},debri.pos2,debri.pos3,debri.vel1,debri.vel2,debri.vel3,))
+        cur.execute('''INSERT OR REPLACE INTO debris (debris_name_id, pos1, pos2, pos3, vel1, vel2, vel3) VALUES (?,?,?,?,?,?,?)''',(debris_id,debri.pos1,debri.pos2,debri.pos3,debri.vel1,debri.vel2,debri.vel3,))
         con.commit()
     except:
-        # # reset_debri()
-        # # try:
-        # #     con = sqlite3.connect('../data/database.db')
-        # #     cur = con.cursor()
-        # #     debris_id = get_debris_id(debri.name)
-        # #     cur.execute('''INSERT OR REPLACE INTO debris VALUES ('{debri.name}',{debri.pos1},{debri.pos2},{debri.pos3},{debri.vel1},{debri.vel2},{debri.vel3})''')
-        # #     con.commit()
-        # except:
         print("ERROR INSERTING DEBRIS VALUE!!!!!!")
     con.close()
 
@@ -73,14 +85,6 @@ def insert_tle(tle):
         cur.execute("""INSERT OR REPLACE INTO tle (debris_name_id,s,t) VALUES (?,?,?)""",(tle_id,tle.s,tle.t,))
         con.commit()
     except:
-        # reset_tle()
-        # try:
-        #     tle_id = get_debris_id(tle.name)
-        #     con = sqlite3.connect('../data/database.db')
-        #     cur = con.cursor()
-        #     cur.execute('''INSERT OR REPLACE INTO tle VALUES ('{tle.name}','{tle.s}','{tle.t}')''')
-        #     con.commit()
-        # except:
         print("ERROR INSERTING TLE VALUE!!!!!!")
     con.close()
 

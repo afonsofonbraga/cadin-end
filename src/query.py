2a787309d5594e3c963cd6c42339b1deefbc6fad
Afonso Braga <afonso.braga@engenharia.ufjf.br>
2021-10-02 10:21:24 -0300

    add table

--- src/query.py
+++ src/query.py
@@ -1,1 +1,16 @@
-#teste
+import sqlite3
+
+def reset_table():
+    con = sqlite3.connect('/database.db')
+    cur = con.cursor()
+    try:
+        cur.execute('''DROP TABLE debris''')
+    except: pass
+
+    cur.execute('''CREATE TABLE debris (name text, s text, t text)''')
+
+    con.commit()
+
+    con.close()
+
+reset_table()

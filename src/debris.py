34c5ef1281dc36df323144111e4fd111ca142fc6
Vitim. .Axlash <giudice.vitor@engenharia.ufjf.br>
2021-10-02 12:26:01 -0300

    try again

--- src/debris.py
+++ src/debris.py
@@ -1,12 +1,14 @@
 class Debris:
-    def __init__(self) -> None:
-        self.name
-        self.type
-        self.s
-        self.t
+    def __init__(self, name, s, t) -> None:
+        self.name = name
+        self.s = s
+        self.t = t
         
-    def __repr__(self):
-        # return "Debri" + self.name
-        # print("Line 1" + self.s)
-        # print("Line 2" + self.t)
-        return f'Debri {self.name}'
+    # def __repr__(self):
+    #     # return "Debri" + self.name
+    #     # print("Line 1" + self.s)
+    #     # print("Line 2" + self.t)
+    #     return f'Debri {self.name}'
+
+    def __repr__(self): 
+        return "Name:% s \n Line1:% s \n Line2:% s \r\n" % (self.name, self.s, self.t) 

d2e9c18222716bd4be234e7a52f2bf36cd4d3c65
Afonso Braga <afonso.braga@engenharia.ufjf.br>
2021-10-02 14:38:06 -0300

    edit treatData

--- src/treatData.py
+++ src/treatData.py
@@ -1,20 +1,32 @@
+from sgp4.api import Satrec
 import sqlite3
+from sgp4.functions import jday
+from query import reset_debri, insert_debri
+from Debris import Debris
 
 def importData():
-    con = sqlite3.connect('../data/database.db')
-    cur = con.cursor()
+    try:
+        con = sqlite3.connect('../data/database.db')
+        cur = con.cursor()
 
-    sqlite_select_query = """SELECT * FROM debris"""
+        sqlite_select_query = """SELECT * FROM tle"""
 
-    cur.execute(sqlite_select_query)
-    records = cur.fetchall()
-    print("Total rows are:  ", len(records))
-    print("Printing each row")
-    for row in records:
-        print("name: ", row[0])
-        print("t: ", row[1])
-        print("s: ", row[2])
-        print("\n")
-    con.close()
+        cur.execute(sqlite_select_query)
+        records = cur.fetchall()
+        con.close()
 
+        print("Total rows are:  ", len(records))
+        print("Printing each row")
+        for row in records:
+            # row[0] -> name
+            # row[1] -> s
+            # row[2] -> t
+            satellite = Satrec.twoline2rv(row[1], row[2])
+            jd, fr = 2458827, 0.362605  #??????????
+            e, r, v = satellite.sgp4(jd, fr)
+            if (e == 0):
+                new_debri = Debris(row[0],r,v)
+                insert_debri(new_debri)
+    except:
+        print("Could not fetch data.")
 importData()

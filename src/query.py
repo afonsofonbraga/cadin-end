ed2b647d5a7b2ddfaab4fc11e4b6f40768ccdba9
Afonso Braga <afonso.braga@engenharia.ufjf.br>
2021-10-02 22:09:08 -0300

    fix code

--- src/query.py
+++ src/query.py
@@ -5,8 +5,8 @@ def create_tables():
     con = sqlite3.connect('../data/database.db')
     cur = con.cursor()
     cur.execute('''CREATE TABLE if not EXISTS debris_name (name_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)''')
-    cur.execute('''CREATE TABLE if not EXISTS debris (debris_name_id INTEGER, pos1 REAL, pos2 REAL, pos3 REAL, vel1 REAL, vel2 REAL, vel3 REAL, FOREIGN KEY(debris_name_id) REFERENCES debris_name(name_id))''')
-    cur.execute('''CREATE TABLE if not EXISTS tle (debris_name_id INTEGER, s TEXT, t TEXT, FOREIGN KEY(debris_name_id) REFERENCES debris_name(name_id))''')
+    cur.execute('''CREATE TABLE if not EXISTS debris (debris_name_id INTEGER PRIMARY KEY, pos1 REAL, pos2 REAL, pos3 REAL, vel1 REAL, vel2 REAL, vel3 REAL, FOREIGN KEY(debris_name_id) REFERENCES debris_name(name_id))''')
+    cur.execute('''CREATE TABLE if not EXISTS tle (debris_name_id INTEGER PRIMARY KEY, s TEXT, t TEXT, FOREIGN KEY(debris_name_id) REFERENCES debris_name(name_id))''')
     con.commit()
     con.close()
 
@@ -59,6 +59,7 @@ def insert_debri(debri):
     try:
         con = sqlite3.connect('../data/database.db')
         cur = con.cursor()
+        print(debris_id)
         cur.execute('''INSERT OR REPLACE INTO debris (debris_name_id, pos1, pos2, pos3, vel1, vel2, vel3) VALUES (?,?,?,?,?,?,?)''',(debris_id,debri.pos1,debri.pos2,debri.pos3,debri.vel1,debri.vel2,debri.vel3,))
         con.commit()
     except:
--- src/treatData.py
+++ src/treatData.py
@@ -1,4 +1,4 @@
-from sgp4.api import Satrec,jday
+from sgp4.api import Satrec
 import sqlite3
 from sgp4.functions import jday
 from query import insert_debri
@@ -7,8 +7,9 @@ from Debris import Debris
 import astropy.time
 from astropy.coordinates import TEME, CartesianDifferential, CartesianRepresentation, ITRS
 from astropy import units as u
+import numpy as np
 
-def importDebris(time):
+def importDebris(time2):
     try:
         con = sqlite3.connect('../data/database.db')
         cur = con.cursor()
@@ -24,40 +25,46 @@ def importDebris(time):
     except:
         print("Could not fetch data from tle database.")
     try:
+        year = int(time2.split(',')[0])
+        month = int(time2.split(',')[1])
+        day = int(time2.split(',')[2])
+        hour = int(time2.split(',')[3])
+        minute = int(time2.split(',')[4])
+        second = int(time2.split(',')[5])
+
+        jd, fr = jday(year, month, day, hour, minute, second)
+
+        time3 = astropy.time.Time(jd + fr, format = 'jd')
+        i = 0
         for row in records:
+            i +=1
             # row[0] -> name_ID
             # row[1] -> s
             # row[2] -> t
+
             satellite = Satrec.twoline2rv(row[1], row[2])
-
-            year = int(time.split(',')[0])
-            month = int(time.split(',')[1])
-            day = int(time.split(',')[2])
-            hour = int(time.split(',')[3])
-            minute = int(time.split(',')[4])
-            second = int(time.split(',')[5])
-
-            jd, fr = jday(year, month, day, hour, minute, second)
             e, r, v = satellite.sgp4(jd, fr)
-
             if e == 0:
                 debris_count += 1
 
-                time = astropy.time.Time(jd + fr, format = 'jd')
+                copyr = np.copy(r)
                 r = CartesianRepresentation(r*u.km)
+                copyv = np.copy(v)
                 v = CartesianDifferential(v*u.km/u.s)
-                teme = TEME(r.with_differentials(v), obstime=time)
-                itrs = teme.transform_to(ITRS(obstime=time))
+                teme = TEME(r.with_differentials(v), obstime=time3)
+                itrs = teme.transform_to(ITRS(obstime=time3))
                 location = itrs.earth_location
+                # print(i)
 
                 longitude = int(str(location.geodetic.lon).split('d')[0])
                 latitude = int(str(location.geodetic.lat).split('d')[0])
                 height = float(str(location.geodetic.height).split('km')[0])
 
                 r = [longitude, latitude, height]
-
-                new_debri = Debris(row[0],r,v)
+                new_debri = Debris(row[0],r,copyv)
                 insert_debri(new_debri)
         print("From", len(records), "tle objects,", debris_count,"debris were added into the database.")
     except:
         print("Probelma nos dados")
+
+importDebris('2021,10,02,21,37,23')

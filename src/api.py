c755665b83b0a43d5692825f5f7e204f79920edd
Afonso Braga <afonso.braga@engenharia.ufjf.br>
2021-10-03 00:06:44 -0300

    add files

---
+++ src/api.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+from get import createJson
+
+app = Flask(__name__)
+api = Api(app)
+
+class Coordinates(Resource):
+    def get(self):
+        data = createJson() # string json que o fonfon ta criando
+
+        return {'data': data }, 200
+
+api.add_resource(Coordinates, '/coordinates')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ src/get.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+import sqlite3
+from query import get_debris_name
+
+db_connect = create_engine('sqlite:///../data/database.db')
+app = Flask(__name__)
+api = Api(app)
+
+def createJson():
+    try:
+        con = sqlite3.connect('../data/database.db')
+        cur = con.cursor()
+
+        sqlite_select_query = """SELECT * FROM debris"""
+
+        cur.execute(sqlite_select_query)
+        records = cur.fetchall()
+        con.close()
+
+        print("Imported", len(records), "tle objects from database.")
+    except:
+        print("Could not fetch data from tle database.")
+    try:
+        for row in records:
+            name = get_debris_name(row[0])
+            alt = row[1]
+            lat = row[2]
+            lon = row[3]
+            a= {
+                'debris_name': name,
+                'altitude': alt,
+                'latitude': lat,
+                'longitude': lon
+            }
+        json2 = dumps(a)
+        return json2
+    except:
+        print("error")
+teste = createJson()
+print(teste)
